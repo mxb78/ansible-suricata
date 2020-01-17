4684c8c20e0aab27d323568df6a4ca482b30cedb
mxb <mxb@unixconn.com>
2020-01-17 13:21:40 +0100

    use python3

--- provision.sh
+++ provision.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 if [ -f /etc/debian_version ]; then
   sudo apt-get update
-  sudo apt-get install -y git python-pip python-dev
+  sudo apt-get install -y git python3-pip python3-dev
 elif [ -f /etc/redhat-release ]; then
-#  rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
   yum install -y epel-release
-  yum install -y git python-pip python-devel python-dnf
+  yum install -y net-tools git python3-pip python3-devel
 fi
-sudo pip install ansible
+sudo pip3 install ansible
 sudo ansible-galaxy install -r /vagrant/requirements.yml -f
-ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
+/usr/local/bin/ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
